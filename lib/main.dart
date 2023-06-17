7a098749f8b9992c64a34e01d6d1fe41f04a1458
mdaffayusuff <95558620+mdaffayusuff@users.noreply.github.com>
2023-06-17 18:02:00 +0700

    main for login

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:tubes/auth_page.dart';
+// import 'package:tubes/auth_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'login.dart';
 import 'register.dart';
 import 'main2.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'user.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const Main());
+  runApp(Main());
 }
 
 class Main extends StatelessWidget {
-  const Main({super.key});
+  Main({super.key});
+
+  final UserCubit _usercubit = UserCubit();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const AuthPage(),
       title: "FUNDFLEX",
       // debugShowCheckedModeBanner: false,
       routes: {
-        "/login": (context) => const AuthPage(),
+        "/login": (context) =>
+            BlocProvider.value(value: _usercubit, child: const Login()),
         "/register": (context) => const Register(),
-        "/home": (context) => const MainRouting(
+        "/home": (context) => BlocProvider.value(
+            value: _usercubit,
+            child: const MainRouting(
               selectedIndex: 0,
-            ),
-        // "/login":(context)=> Login(),
+            )),
       },
       initialRoute: "/login",
     );
